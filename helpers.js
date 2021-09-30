0f097211d630f5e75a09d4d023d0b264a1c1fc79
Adriana <adriana.calvomatos@gmail.com>
2021-09-30 00:55:56 +0000

    add M&C tests for helpers.js

--- helpers.js
+++ helpers.js
@@ -1,7 +1,7 @@
 const getUserByEmail = function (email, database) {
   for (let key in database) {
     if (database[key].email === email) {
-      return database[key].id;
+      return key;
     }
   }
 };
